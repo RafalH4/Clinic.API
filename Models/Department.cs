447d70ba325ed1c44d188cf586097429b9378440
Rafał Huk <r.huk@outlook.com>
2019-10-10 11:24:50 +0200

    Implementacja części modeli

---
+++ Models/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clinic.API.Models
+{
+    public class Department
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Descriptcion { get; set; }
+        public List<Contract> Contracts { get; set; }
+        public List<MedOffice> MedOffices { get; set; }
+    }
+}
