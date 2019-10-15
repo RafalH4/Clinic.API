37310a6a847baca1d68e6dcf57d8cbe7279d9d73
Rafał Huk <r.huk@outlook.com>
2019-10-15 14:25:11 +0200

    model klas

---
+++ Models/Drug.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clinic.API.Models
+{
+    public class Drug
+    {
+        public List<Prescription> Prescriptions { get; set; }
+    }
+}
