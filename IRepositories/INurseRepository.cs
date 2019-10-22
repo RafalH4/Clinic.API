80f736e78c281f164468ef0fe25754e7b8e5f22a
Rafał Huk <r.huk@outlook.com>
2019-10-22 22:39:26 +0200

    NurseRepository

--- IRepositories/INurseRepository.cs
+++ IRepositories/INurseRepository.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using Clinic.API.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Clinic.API.IRepositories
 {
-    interface INurseRepository
+   public interface INurseRepository
     {
+        Task<IEnumerable<Nurse>> Get();
+        Task<Nurse> GetById(Guid id);
+        Task<Nurse> GetByEmail(string email);
+        Task<Nurse> GetByPesel(string pesel);
+        Task AddNurse(Nurse nurse);
+        Task UpdateNurse(Nurse nurse);
+        Task DeleteNurse(Nurse nurse);
     }
 }
---
+++ Repositories/NurseRepository.cs
@@ -0,0 +1,57 @@
+﻿using Clinic.API.Data;
+using Clinic.API.IRepositories;
+using Clinic.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clinic.API.Repositories
+{
+    public class NurseRepository : INurseRepository
+    {
+        private readonly DataContext _context;
+        public NurseRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task AddNurse(Nurse nurse)
+        {
+            await _context.Nurses.AddAsync(nurse);
+            await _context.SaveChangesAsync();
+            await Task.CompletedTask;
+        }
+
+        public async Task DeleteNurse(Nurse nurse)
+        {
+            _context.Nurses.Remove(nurse);
+            await _context.SaveChangesAsync();
+            await Task.CompletedTask;
+        }
+
+        public async Task<IEnumerable<Nurse>> Get()
+        {
+            var nurses = _context.Nurses.ToList();
+            return await Task.FromResult(nurses);
+        }
+
+        public async Task<Nurse> GetByEmail(string email)
+            => await Task.FromResult(_context.Nurses.SingleOrDefault(
+                nurse => nurse.Email == email));
+
+        public async Task<Nurse> GetById(Guid id)
+            => await Task.FromResult(_context.Nurses.SingleOrDefault(
+                nurse => nurse.Id == id));
+
+        public async Task<Nurse> GetByPesel(string pesel)
+            => await Task.FromResult(_context.Nurses.SingleOrDefault(
+                nurse => nurse.Pesel == pesel));
+
+        public async Task UpdateNurse(Nurse nurse)
+        {
+            _context.Nurses.Update(nurse);
+            await _context.SaveChangesAsync();
+            await Task.CompletedTask;
+        }
+    }
+}
