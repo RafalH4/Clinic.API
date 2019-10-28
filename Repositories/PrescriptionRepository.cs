9bc5dbacb79e767f03cc884597e86e59c63fc16b
Rafał Huk <r.huk@outlook.com>
2019-10-28 18:14:20 +0100

    RefferalRepository i PresriptionRepository

---
+++ Repositories/PrescriptionRepository.cs
@@ -0,0 +1,54 @@
+﻿using Clinic.API.Data;
+using Clinic.API.IRepositories;
+using Clinic.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clinic.API.Repositories
+{
+    public class PrescriptionRepository : IPrescriptionRepository
+    {
+        private readonly DataContext _context;
+        public PrescriptionRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Prescription>> Get()
+            => await Task.FromResult(_context.Prescriptions.ToList());
+
+        public async Task<IEnumerable<Prescription>> GetByAppointment(Appointment appointment)
+            => await Task.FromResult(_context.Prescriptions
+                .Where(prescription => prescription.Appointment.Equals(appointment)).ToList());
+
+        public async Task<IEnumerable<Prescription>> GetByDate(DateTime date)
+            => await Task.FromResult(_context.Prescriptions
+                .Where(prescription => prescription.CreatedAt.Equals(date)).ToList());
+
+        public async Task<Prescription> GetById(Guid id)
+            => await Task.FromResult(_context.Prescriptions
+                .SingleOrDefault(prescription => prescription.Id.Equals(id)));
+
+        public async Task AddPrescription(Prescription prescription)
+        {
+            await _context.Prescriptions.AddAsync(prescription);
+            await _context.SaveChangesAsync();
+            await Task.CompletedTask;
+        }
+
+        public async Task DeletePrescription(Prescription prescription)
+        {
+            _context.Prescriptions.Remove(prescription);
+            await _context.SaveChangesAsync();
+            await Task.CompletedTask;
+        }
+                
+        public async Task UpdatePrescription(Prescription prescription)
+        {
+            _context.Prescriptions.Update(prescription);
+            await _context.SaveChangesAsync();
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ Repositories/ReferralRepository.cs
@@ -0,0 +1,53 @@
+﻿using Clinic.API.Data;
+using Clinic.API.IRepositories;
+using Clinic.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clinic.API.Repositories
+{
+    public class ReferralRepository : IReferralRepository
+    {
+        private readonly DataContext _context;
+        public ReferralRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Referral>> Get()
+            => await Task.FromResult(_context.Referrals.ToList());
+
+        public async Task<IEnumerable<Referral>> GetByAppointment(Appointment appointment)
+            => await Task.FromResult(_context.Referrals
+                .Where(referral => referral.Appointment.Equals(appointment)).ToList());
+
+        public async Task<IEnumerable<Referral>> GetByDate(DateTime date)
+            => await Task.FromResult(_context.Referrals
+                .Where(referral => referral.CreatedAt.Equals(date)).ToList());
+
+        public async Task<Referral> GetById(Guid id)
+            => await Task.FromResult(_context.Referrals
+                .SingleOrDefault(referral => referral.Id.Equals(id)));
+        public async Task AddReferral(Referral referral)
+        {
+            await _context.Referrals.AddAsync(referral);
+            await _context.SaveChangesAsync();
+            await Task.CompletedTask;
+        }
+
+        public async Task DeleteReferral(Referral referral)
+        {
+            _context.Referrals.Remove(referral);
+            await _context.SaveChangesAsync();
+            await Task.CompletedTask;
+        }
+
+        public async Task UpdateReferral(Referral referral)
+        {
+            _context.Referrals.Update(referral);
+            await _context.SaveChangesAsync();
+            await Task.CompletedTask;
+        }
+    }
+}
